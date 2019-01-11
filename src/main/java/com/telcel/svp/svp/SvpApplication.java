3b93c23b8e59a3ad642e13ebf406bcb3ed95d6f4
megumiEvans <m3gum1t4>
2019-01-11 00:34:05 -0600

    First commit, adding spring boot skeleton

---
+++ src/main/java/com/telcel/svp/svp/SvpApplication.java
@@ -0,0 +1,14 @@
+package com.telcel.svp.svp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvpApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/telcel/svp/svp/SvpApplicationTests.java
@@ -0,0 +1,17 @@
+package com.telcel.svp.svp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SvpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
